724279069cb48db97195fecd298758d0daffa7f9
Per Odlund <per.odlund@gmail.com>
2014-07-30 09:42:13 +0200

    Make it possible to change quality

--- twitch.py
+++ twitch.py
@@ -6,7 +6,11 @@ import simplejson as json
 response = urllib.request.urlopen('https://api.twitch.tv/kraken/streams/' + sys.argv[1])
 html = response.read().decode("utf-8")
 j = json.loads(html)
+if len(sys.argv) == 3:
+    quality = sys.argv[2]
+else:
+    quality = 'medium'
 if j['stream']:
-    print('livestreamer -p mpv ' + j['stream']['channel']['url'] + ' medium')
+    print('livestreamer -p mpv ' + j['stream']['channel']['url'] + ' ' + quality)
 else:
     print('#Offline')
